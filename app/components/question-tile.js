bbcbe5fe7d19f38bb075d30bd9e83271da959d28
Koji Nakagawa <lightupthesky0627@gmail.com>
2017-03-24 11:15:38 -0700

    add question detail, add question route and refactor code

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,15 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     actions: {
 
-      update(question, params){
-        //passed from update-question.js, pass to index.hbs
-          this.sendAction('update', question, params);
-      },
+      // update(question, params){
+      //   //passed from update-question.js, pass to index.hbs
+      //     this.sendAction('update', question, params);
+      // },
 
-      delete(question) {
-        if(confirm('Are you sure you want to delete this question?')) {
-          this.sendAction("destroyQuestion", question);
-        }
-      }
+
     }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -6,29 +6,30 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    destroyQuestion(question) {
-      question.destroyRecord();
-      this.transitionTo('index');
-    },
-
     saveQuestion(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
       this.transitionTo('index');
     },
 
-    update(question, params){
-      console.log(question);
-      console.log(params);
-      Object.keys(params).forEach(function(key){
-        if(params[key]!==undefined){
-          question.set(key, params[key]);
-          console.log("true");
-        }
-      });
-      question.save();
-      this.transitionTo('index');
-    }
+    // destroyQuestion(question) {
+    //   question.destroyRecord();
+    //   this.transitionTo('index');
+    // },
+    //
+    //
+    // update(question, params){
+    //   console.log(question);
+    //   console.log(params);
+    //   Object.keys(params).forEach(function(key){
+    //     if(params[key]!==undefined){
+    //       question.set(key, params[key]);
+    //       console.log("true");
+    //     }
+    //   });
+    //   question.save();
+    //   this.transitionTo('index');
+    // }
 
   }
 
